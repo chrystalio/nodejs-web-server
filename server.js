f3abf859d055d698c94607549d8d2a3809117c6f
chrystalio <chrystalio.k@outlook.com>
2023-02-12 03:56:25 +0700

    Add handling request type GET, POST, PUT, DELETE

--- server.js
+++ server.js
@@ -1,10 +1,26 @@
 const http = require('http');
 
 const requestListener = function (req, res) {
-  res.setHeader("Content-Type", "text/html");
+    res.setHeader("Content-Type", "text/html");
 
-  res.statusCode = 200;
-  res.end(`<h1>Hello, HTTP Server!</h1>`);
+    res.statusCode = 200;
+    const {method} = req;
+
+    if (method === 'GET') {
+        res.end(`<h1>Hello, HTTP Server! [GET]</h1>`);
+    }
+
+    if (method === 'POST') {
+        res.end(`<h1>Hello, HTTP Server! [POST]</h1>`);
+    }
+
+    if (method === 'PUT') {
+        res.end(`<h1>Hello, HTTP Server! [PUT]</h1>`);
+    }
+
+    if (method === 'DELETE') {
+        res.end(`<h1>Hello, HTTP Server! [DELETE]</h1>`);
+    }
 };
 
 const server = http.createServer(requestListener);
@@ -13,5 +29,5 @@ const port = 5000;
 const host = 'localhost';
 
 server.listen(port, host, () => {
-  console.log(`Server is running on http://${host}:${port}`);
-});GOIT
+    console.log(`Server is running on http://${host}:${port}`);
+});
