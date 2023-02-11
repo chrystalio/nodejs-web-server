d5e5de1e94859021e3be8459be69322f4ceb943b
chrystalio <chrystalio.k@outlook.com>
2023-02-12 03:51:42 +0700

    Configure and create running server

--- server.js
+++ server.js
@@ -1,1 +1,17 @@
-console.log('Halo Dunia!, Saya sedang belajar membuat server menggunakan Node.js');
+const http = require('http');
+
+const requestListener = function (req, res) {
+  res.setHeader("Content-Type", "text/html");
+
+  res.statusCode = 200;
+  res.end(`<h1>Hello, HTTP Server!</h1>`);
+};
+
+const server = http.createServer(requestListener);
+
+const port = 5000;
+const host = 'localhost';
+
+server.listen(port, host, () => {
+  console.log(`Server is running on http://${host}:${port}`);
+});GOIT
