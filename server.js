15d7259c253c1c476af2558f52f727ddf7de7ab5
chrystalio <chrystalio.k@outlook.com>
2023-02-13 13:46:39 +0700

    Define server response status

--- server.js
+++ server.js
@@ -8,39 +8,38 @@ const requestListener = function (req, res) {
 
     if (url === '/') {
         if (method === 'GET') {
+            res.statusCode = 200;
             res.end(`<h1>Halo! Ini adalah halaman home</h1>`);
         } else {
+            res.statusCode = 400;
             res.end(`<h1>Halaman tidak dapat diakses dengan ${method} request</h1>`);
         }
     }
 
     if (url === '/about') {
         if (method === 'GET') {
+            res.statusCode = 200;
             res.end(`<h1>Halo! Ini adalah halaman about</h1>`);
         } else if (method === 'POST') {
-        }
+            let body = [];
 
-        if (url === '/about') {
-            if (method === 'GET') {
-                res.end(`<h1>Halo! Ini adalah halaman about</h1>`);
-            } else if (method === 'POST') {
-                let body = [];
+            req.on('data', (chunk) => {
+                body.push(chunk);
+            });
 
-                req.on('data', (chunk) => {
-                    body.push(chunk);
-                });
-
-                req.on('end', () => {
-                    body = Buffer.concat(body).toString();
-                    const {name} = JSON.parse(body);
-                    res.end(`<h1>Hello, ${name}! Ini adalah halaman about</h1>`);
-                });
-            } else {
-                res.end(`<h1>Halaman tidak dapat diakses dengan ${method} request</h1>`);
-            }
+            req.on('end', () => {
+                body = Buffer.concat(body).toString();
+                const {name} = JSON.parse(body);
+                res.statusCode = 200;
+                res.end(`<h1>Hello, ${name}! Ini adalah halaman about</h1>`);
+            });
         } else {
-            res.end(`<h1>Halaman Tidak Ditemukan!</h1>`)
+            res.statusCode = 400
+            res.end(`<h1>Halaman tidak dapat diakses dengan ${method} request</h1>`);
         }
+    } else {
+        res.statusCode = 400
+        res.end(`<h1>Halaman Tidak Ditemukan!</h1>`)
     }
 };
 
